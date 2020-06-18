3ab1ff5d7234c592d536c5fbe36ed0bf07e58a4f
ntbased <danielkyleluo99@gmail.com>
2020-06-17 20:41:59 -0700

    update

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ client.on('message', msg => {
  if (msg.content === 'broom') {
  msg.channel.send('i am broom');
  }
+
  if (msg.content.includes('scary')) {
+   const scaryList = ["alarming", "chilling", "creepy", "eerie", "hairy", "horrifying", "intimidating", "shocking", "spooky", "bloodcurdling", "hair-raising", "horrendous", "spine-chilling", "bone-chilling", "unnverving", "frightening", "terrifying", "you're scary"]
    if (msg.author.bot === false) {
-   if (Math.floor(Math.random()*10) < 2) {
-      msg.channel.send("you're scary");
+   if (Math.floor(Math.random()*10) < 1) {
+      msg.channel.send(scaryList[Math.floor(Math.random() * scaryList.length)]);
    }
 }
  }
 
  if (msg.content === 'number') {
    msg.channel.send(Math.floor(Math.random()*10));
-   msg.react("💩");
  }
  if (msg.content === 'poop') {
    msg.react("💩");
@@ -31,7 +32,7 @@ client.on('message', msg => {
 
  if (msg.content.includes('damn')) {
       if (msg.author.bot === false) {
-   if (Math.floor(Math.random()*10) < 2) {
+   if (Math.floor(Math.random()*10) < 3) {
    msg.channel.send("<@" + msg.author.id + ">" + " says: " + msg.content.replace("damn", "I'm frustrated"));
  }
 }
@@ -39,7 +40,7 @@ client.on('message', msg => {
 
  if (msg.content.includes('wtf')) {
       if (msg.author.bot === false) {
-   if (Math.floor(Math.random()*10) < 2) {
+   if (Math.floor(Math.random()*10) < 3) {
    msg.channel.send("<@" + msg.author.id + ">" + " says: " + msg.content.replace("wtf", "I don't understand"));
  }
 }
@@ -50,6 +51,17 @@ if (msg.content.startsWith("spanishify")) {
       var combined = ""
       for (i = 1; i < msgsplit.length; i++) {
         combined = combined + " " + msgsplit[i] + "o"
+
+    }
+    msg.channel.send(combined);
+  }
+}
+if (msg.content.startsWith("japanify")) {
+     if (msg.author.bot === false) {
+    	var msgsplit = msg.content.split(" ");
+      var combined = ""
+      for (i = 1; i < msgsplit.length; i++) {
+        combined = combined + " " + msgsplit[i] + "u"
 
     }
     msg.channel.send(combined);
