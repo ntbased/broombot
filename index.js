e73e4a4bfab5ae94701c554d9387a11807efd63e
ntbased <danielkyleluo99@gmail.com>
2020-06-17 18:03:14 -0700

    first

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+ const client = new Discord.Client();
+ const { token } = require('./token.json')
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'broombot') {
+ msg.channel.send('i am broom');
+ }
+ if (msg.content === 'broom') {
+ msg.channel.send('i am broom');
+ }
+ if (msg.content.includes('scary')) {
+   if (Math.floor(Math.random()*10) < 4) {
+      msg.channel.send('youre scary');
+   }
+ }
+ if (msg.content === 'number') {
+   msg.channel.send(Math.floor(Math.random()*10));
+   msg.react("💩");
+ }
+ if (msg.content === 'poop') {
+   msg.react("💩");
+ }
+
+ if (msg.content.includes('damn')) {
+   if (Math.floor(Math.random()*10) < 4) {
+   msg.channel.send("<@" + msg.author.id + ">" + " says: " + msg.content.replace("damn", "I'm frustrated"));
+ }
+}
+
+ if (msg.content.includes('wtf')) {
+   if (Math.floor(Math.random()*10) < 4) {
+   msg.channel.send("<@" + msg.author.id + ">" + " says: " + msg.content.replace("wtf", "I don't understand"));
+ }
+}
+if (msg.content.startsWith("test")) {
+    	var msgsplit = msg.content.split(" ");
+      var combined = ""
+      for (i = 1; i < msgsplit.length; i++) {
+        combined = combined + " " + msgsplit[i] + "o"
+
+    }
+    msg.channel.send(combined);
+  }
+
+ });
+
+client.login(token);
